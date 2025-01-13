14a5031a949a0dd96c118aa8b1e4db3eb950fa13
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-13 14:09:45 -0300

    datos componente hero-item

--- src/app/components/hero-item/hero-item.component.ts
+++ src/app/components/hero-item/hero-item.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './hero-item.component.scss'
 })
 export class HeroItemComponent {
-
+  hero = {
+    id: 620,
+    name: 'Spider-Man',
+    powerstats: {
+      intelligence: 90,
+      strenght: 55,
+      speed: 67,
+      durability: 75,
+      power: 74,
+      combat: 85
+    },
+    image: '',
+    alignment: 'good'
+  }
 }
