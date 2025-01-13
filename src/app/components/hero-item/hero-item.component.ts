b2d75b736791ba931219abb2036f7416eff0df31
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-13 13:35:45 -0300

    creo componente

---
+++ src/app/components/hero-item/hero-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-item',
+  imports: [],
+  templateUrl: './hero-item.component.html',
+  styleUrl: './hero-item.component.scss'
+})
+export class HeroItemComponent {
+
+}
