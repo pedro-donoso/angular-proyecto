237f9931efad31a4aeb36f450f01f79e289a3cee
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-13 23:45:23 -0300

    agrego estadisticas lista de heroes

--- src/app/components/hero-list/hero-list.component.ts
+++ src/app/components/hero-list/hero-list.component.ts
@@ -5,8 +5,54 @@ import { HeroItemComponent } from "../hero-item/hero-item.component";
   selector: 'app-hero-list',
   imports: [HeroItemComponent],
   templateUrl: './hero-list.component.html',
-  styleUrl: './hero-list.component.scss'
+  styleUrl: './hero-list.component.scss',
 })
 export class HeroListComponent {
-
+  public heroes: Hero[] = [
+    {
+      id: 620,
+      name: 'Spider-Man',
+      powerstats: {
+        intelligence: 90,
+        strength: 55,
+        speed: 67,
+        durability: 75,
+        power: 74,
+        combat: 85,
+      },
+      image:
+        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/620-spider-man.jpg',
+      alignment: 'good',
+    },
+    {
+      id: 225,
+      name: 'Doctor Octopus',
+      powerstats: {
+        intelligence: 94,
+        strength: 48,
+        speed: 33,
+        durability: 40,
+        power: 53,
+        combat: 65,
+      },
+      image:
+        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg',
+      alignment: 'bad',
+    },
+    {
+      id: 70,
+      name: 'Batman',
+      powerstats: {
+        intelligence: 100,
+        strength: 26,
+        speed: 27,
+        durability: 50,
+        power: 47,
+        combat: 100,
+      },
+      image:
+        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg',
+      alignment: 'good',
+    }
+  ];
 }
