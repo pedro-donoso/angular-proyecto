0556d281b6d6534de220e8073b0f41f49bf55787
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-14 10:36:52 -0300

    itero estadisticas

--- src/app/components/hero-item/hero-item.component.ts
+++ src/app/components/hero-item/hero-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, input, output } from '@angular/core';
 import { Hero, PowerStat } from '../../../../shared/interfaces/hero.interface';
 import { HeroPowerstatsChange } from '../../../../shared/interfaces/hero-powerstats-change.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-item.component.html',
   styleUrl: './hero-item.component.scss',
 })
--- src/app/components/hero-list/hero-list.component.ts
+++ src/app/components/hero-list/hero-list.component.ts
@@ -11,51 +11,51 @@ import { HeroPowerstatsChange } from '../../../../shared/interfaces/hero-powerst
 })
 export class HeroListComponent {
   public heroes: Hero[] = [
-    // {
-    //   id: 620,
-    //   name: 'Spider-Man',
-    //   powerstats: {
-    //     intelligence: 90,
-    //     strength: 55,
-    //     speed: 67,
-    //     durability: 75,
-    //     power: 74,
-    //     combat: 85,
-    //   },
-    //   image:
-    //     'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/620-spider-man.jpg',
-    //   soul: 'good',
-    // },
-    // {
-    //   id: 637,
-    //   name: 'Steppenwolf',
-    //   powerstats: {
-    //     intelligence: 94,
-    //     strength: 100,
-    //     speed: 83,
-    //     durability: 100,
-    //     power: 100,
-    //     combat: 100,
-    //   },
-    //   image:
-    //     'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/637-steppenwolf.jpg',
-    //   soul: 'bad',
-    // },
-    // {
-    //   id: 70,
-    //   name: 'Batman',
-    //   powerstats: {
-    //     intelligence: 100,
-    //     strength: 26,
-    //     speed: 27,
-    //     durability: 50,
-    //     power: 47,
-    //     combat: 100,
-    //   },
-    //   image:
-    //     'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg',
-    //   soul: 'good',
-    // },
+    {
+      id: 620,
+      name: 'Spider-Man',
+      powerstats: {
+        intelligence: 90,
+        strength: 55,
+        speed: 67,
+        durability: 75,
+        power: 74,
+        combat: 85,
+      },
+      image:
+        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/620-spider-man.jpg',
+      soul: 'good',
+    },
+    {
+      id: 637,
+      name: 'Steppenwolf',
+      powerstats: {
+        intelligence: 94,
+        strength: 100,
+        speed: 83,
+        durability: 100,
+        power: 100,
+        combat: 100,
+      },
+      image:
+        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/637-steppenwolf.jpg',
+      soul: 'bad',
+    },
+    {
+      id: 70,
+      name: 'Batman',
+      powerstats: {
+        intelligence: 100,
+        strength: 26,
+        speed: 27,
+        durability: 50,
+        power: 47,
+        combat: 100,
+      },
+      image:
+        'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/70-batman.jpg',
+      soul: 'good',
+    },
   ];
   savePowerstats({hero, powerstat, value}: HeroPowerstatsChange) {
     hero.powerstats[powerstat] += value;
