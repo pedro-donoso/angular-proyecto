b6461b415facabb0e18f57b08f524023e88d33d0
pedro-donoso <pedro.donoso.r@usach.cl>
2025-01-13 23:19:05 -0300

    agrego lista de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroItemComponent } from './components/hero-item/hero-item.component';
+import { HeroListComponent } from "./components/hero-list/hero-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeroItemComponent],
+  imports: [HeroItemComponent, HeroListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/hero-list/hero-list.component.ts
+++ src/app/components/hero-list/hero-list.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroItemComponent } from "../hero-item/hero-item.component";
 
 @Component({
   selector: 'app-hero-list',
-  imports: [],
+  imports: [HeroItemComponent],
   templateUrl: './hero-list.component.html',
   styleUrl: './hero-list.component.scss'
 })
